a7ea7ad9e62c6b948f141549f4f527667e0d308b
cppppppp7 <cp@artela.network>
2023-12-11 19:29:22 +0800

    add query panel

--- js_client/session_key_aspect_client/test/client/src/App.js
+++ js_client/session_key_aspect_client/test/client/src/App.js
@@ -6,15 +6,13 @@ const SessionKeyAspectClient = require('sessioin-key-aspect-client');
 
 const App = () => {
 
-  const [walletAddress, setWalletAddress] = useState('');
-
   const testContract = "0330032b4a11478969dc4caaa11ecc2ea98cfcFF";
   const testMethods = ["0A0A0A0A", "0B0B0B0B"];
   const testAspectAddress = "0x06786bB59719d7DDD9D42457a16BbCD6953A7cab";
 
-  const web3 = new Web3(window.ethereum);
-  const aspectClient = new SessionKeyAspectClient(web3, testAspectAddress);
 
+  const [aspectAddress, setAspectAddress] = useState(testAspectAddress);
+  const [walletAddress, setWalletAddress] = useState('');
   const [sessionKey, setSessionKey] = useState(generateRandomEthereumAddress());
   const [syncData, setSyncData] = useState({
     walletAddress: '',
@@ -23,6 +21,14 @@ const App = () => {
     bindingMethodSet: [],
     expireBlockHeight: ''
   });
+
+  const [mainKeyQP, setMainKeyQP] = useState('');
+  const [sessionKeyQP, setSessionKeyQP] = useState('');
+  const [contractQP, setContractQP] = useState('');
+  const [queryResultQP, setQueryResultQP] = useState({});
+
+  const web3 = new Web3(window.ethereum);
+  const aspectClient = new SessionKeyAspectClient(web3, aspectAddress);
 
   useEffect(() => {
 
@@ -59,6 +65,11 @@ const App = () => {
     return () => clearInterval(intervalId);
   }, [sessionKey, walletAddress]);
 
+  const handleQuery = async () => {
+    const data = await aspectClient.getSessionKey(mainKeyQP, sessionKeyQP, contractQP);
+    setQueryResultQP(data);
+  };
+
   function generateRandomEthereumAddress() {
     let address = '0x';
     const characters = '0123456789abcdef';
@@ -80,32 +91,85 @@ const App = () => {
   };
 
   return (
-    <div className="center-container">
-      <p className="title">Connected Wallet</p>
-      <p>{walletAddress}</p>
-      <p className="title">Random session key</p>
-      <div className="input-container">
-        <input
-          type="text"
-          value={sessionKey}
-          onChange={(e) => {
-            console.log("e.target.value", e.target.value);
-            setSessionKey(e.target.value)
-          }}
-          className="session-key-input"
-        />
+    <div>
+      <div className="center-container-aspect">
+        <p className="title">Aspect</p>
+        <div className="input-container">
+          <input
+            type="text"
+            value={aspectAddress}
+            onChange={(e) => {
+              setAspectAddress(e.target.value)
+            }}
+            className="session-key-input"
+          />
+        </div>
       </div>
-      <p className="title">Binding contract and methods</p>
-      <p>{"0x" + testContract}</p>
-      <p> {"[" + testMethods[0] + "," + testMethods[1] + "]"} </p>
-      <div className="button-container">
-        <button className="register-button" onClick={handleRegister}>register</button>
+      <div className='container'>
+        <div className="center-container">
+          <p className="title-h1">Query session key</p>
+          <p className="title">param1: main key address</p>
+          <input
+            type="text"
+            placeholder="0xaabb...ccdd"
+            value={mainKeyQP}
+            onChange={e => setMainKeyQP(e.target.value)}
+            className="session-key-input"
+          />
+          <p className="title">param2: session key address</p>
+          <input
+            type="text"
+            placeholder="0xaabb...ccdd"
+            value={sessionKeyQP}
+            onChange={e => setSessionKeyQP(e.target.value)}
+            className="session-key-input"
+          />
+          <p className="title">param3: contract address</p>
+          <input
+            type="text"
+            placeholder="0xaabb...ccdd"
+            value={contractQP}
+            onChange={e => setContractQP(e.target.value)}
+            className="session-key-input"
+          />
+          <p className="title"></p>
+          <button className="register-button" onClick={handleQuery}>Query</button>
+          <div>
+            <pre>{JSON.stringify(queryResultQP, null, 2)}</pre>
+          </div>
+        </div>
+
+        <div className="center-container">
+          <p className="title-h1">Test registered by Metamask</p>
+          <p className="title">Connected Wallet</p>
+          <p>{walletAddress}</p>
+          <p className="title">Random session key</p>
+          <div className="input-container">
+            <input
+              type="text"
+              value={sessionKey}
+              onChange={(e) => {
+                console.log("e.target.value", e.target.value);
+                setSessionKey(e.target.value)
+              }}
+              className="session-key-input"
+            />
+          </div>
+          <p className="title">Binding contract and methods</p>
+          <p>{"0x" + testContract}</p>
+          <p> {"[" + testMethods[0] + "," + testMethods[1] + "]"} </p>
+          <div className="button-container">
+            <button className="register-button" onClick={handleRegister}>register</button>
+          </div>
+          <p className="title">Query this session key from blockchain</p>
+          <div className="info-box">
+            <pre>{JSON.stringify(syncData, null, 2)}</pre>
+          </div>
+        </div>
       </div>
-      <p className="title">Query this session key from blockchain</p>
-      <div className="info-box">
-        <pre>{JSON.stringify(syncData, null, 2)}</pre>
-      </div>
+
     </div>
+
   );
 }
 
